9c570f364638e856c6c25f67751b544698fca35a
Masonrapa <33574271+masonrapa@users.noreply.github.com>
2020-09-10 15:46:51 +0200

    MCM
    Minecraft Command Maker

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (this.Contenedor.Controls.Count > 0)
+                this.Contenedor.Controls.RemoveAt(0);
+            Form fh = new Form0() as Form;
+            fh.TopLevel = false;
+            fh.Dock = DockStyle.Fill;
+            this.Contenedor.Controls.Add(fh);
+            this.Contenedor.Tag = fh;
+            fh.Show();
+        }
+
+        private void Contenedor_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
